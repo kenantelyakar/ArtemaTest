aac666c27b79bc90528ed83e4149898f7e546fc2
kenantelyakar <kenantelyakar@gmail.com>
2023-09-11 14:42:22 +0300

    postgres connection success

--- NodeJS/ComponentRead/db-conn.js
+++ NodeJS/ComponentRead/db-conn.js
@@ -1,6 +1,18 @@
 'use strict';
 const xsenv = require('@sap/xsenv');
-
+const pgp = require('pg-promise')();
+let dbConf = {
+    host: '',
+    port: '',
+    database:'' ,
+    user: '',
+    password: '',
+    ssl: {
+        rejectUnauthorized : false,
+        ca   : '',
+  }
+}
+let sslCert = '';
 const createTable =
     'CREATE TABLE IF NOT EXISTS Users \
         ( \
@@ -18,7 +30,14 @@ function returnUriToDB() {
     var uri = '';
     if (process.env.VCAP_SERVICES) {
         // running in cloud
-        uri = xsenv.cfServiceCredentials('postgresql-cf').uri;
+        //uri = xsenv.cfServiceCredentials('postgresql-cf').uri;
+        //sslCert = xsenv.cfServiceCredentials('postgresql-cf').sslcert;
+        dbConf.host = xsenv.cfServiceCredentials('postgresql-cf').hostname;
+        dbConf.port = xsenv.cfServiceCredentials('postgresql-cf').port;
+        dbConf.database = xsenv.cfServiceCredentials('postgresql-cf').dbname;
+        dbConf.user = xsenv.cfServiceCredentials('postgresql-cf').username;
+        dbConf.password = xsenv.cfServiceCredentials('postgresql-cf').password;
+        dbConf.ssl.ca = xsenv.cfServiceCredentials('postgresql-cf').sslcert;
     } else {
         console.log('running locally is not supported');
     }
@@ -27,11 +46,8 @@ function returnUriToDB() {
 
 
 function getDB(cb) {
-    let pgp = require('pg-promise')({
-        ssl:true
-    });
-    
-    var db = pgp(returnUriToDB());
+    returnUriToDB();
+    var db = pgp(dbConf);
     let sql = createTable;
     db.query(sql)
         .then(function () {
@@ -44,6 +60,7 @@ function getDB(cb) {
             cb(err, null);
             return;
         });
+        cb(null, db);
 }
 
 module.exports = {
