9e6689177bb17cb90f3c2b1d54e7ad79d417f51c
kenantelyakar <kenantelyakar@gmail.com>
2023-09-21 13:59:52 +0300

    Delete TypeScript/src/apisdk directory

--- TypeScript/src/apisdk/sapdme_labor/schema/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
- *
- * This is a generated file powered by the SAP Cloud SDK for JavaScript.
- */
-export * from './error-response';
-export * from './labor-report';
-export * from './labor-report-request';
--- TypeScript/src/apisdk/sapdme_notification/schema/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
- *
- * This is a generated file powered by the SAP Cloud SDK for JavaScript.
- */
-export * from './notification-message';
-export * from './notification-parameter';
-export * from './notification-subscription';
-export * from './error';
